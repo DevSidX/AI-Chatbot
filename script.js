d7312863e92873238ee9834fcea09617dec0206b
SIDDHARTH <121356793+DevSidX@users.noreply.github.com>
2025-11-06 06:45:36 +0000

    api link changed

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector('#chatbot-toggler');
 const closeChatbot = document.querySelector("#close-chatbot");
 
 const API_KEY = "AIzaSyBHfForREVK0weQfFjDIbGt9j417hhtVhY";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
     message: null,
